409b8ff2af6472f05579d2fe320403e52cdc4946
Zenyr <zenyr@zenyr.com>
2018-05-16 00:40:21 +0900

    add test cases

--- index.test.jsx
+++ index.test.jsx
@@ -5,8 +5,62 @@ import Adapter from 'enzyme-adapter-react-16';
 import { PortalProvider, WhitePortal, BlackPortal } from '.';
 
 configure({ adapter: new Adapter() });
+const YourApp = () => <div>ok</div>;
+test('smoke test', () => {
+  const wrapper = mount(
+    <PortalProvider>
+      <YourApp />
+    </PortalProvider>
+  );
+  expect(wrapper.contains('ok')).toBeTruthy();
+  wrapper.unmount();
+});
 
-test('is working', () => {
-  const wrapper = mount(<div>hey</div>);
-  expect(wrapper.contains('hey')).toBeTruthy();
+test('basic', () => {
+  const wrapper = mount(
+    <PortalProvider>
+      <div>
+        a
+        <WhitePortal name="one" />
+        b
+        <BlackPortal name="one">1</BlackPortal>
+        c
+      </div>
+    </PortalProvider>
+  );
+  expect(wrapper.text()).toBe('a1bc');
+  wrapper.unmount();
+});
+
+test('name mismatch', () => {
+  const wrapper = mount(
+    <PortalProvider>
+      <div>
+        a
+        <WhitePortal name="two" />
+        b
+        <BlackPortal name="one">1</BlackPortal>
+        c
+      </div>
+    </PortalProvider>
+  );
+  expect(wrapper.text()).toBe('abc');
+  wrapper.unmount();
+});
+
+test('multiple WhitePortal', () => {
+  const wrapper = mount(
+    <PortalProvider>
+      <div>
+        a
+        <WhitePortal name="one" />
+        <WhitePortal name="one" />
+        b
+        <BlackPortal name="one">1</BlackPortal>
+        c
+      </div>
+    </PortalProvider>
+  );
+  expect(wrapper.text()).toBe('a11bc');
+  wrapper.unmount();
 });
